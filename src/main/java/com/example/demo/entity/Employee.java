a10271da3b1b2ecd601e6c91ba7b7d6a3f200246
Kousei Kanai <kanai@tdc.co.jp>
2025-03-06 11:13:20 +0900

    コンストラクタの追加

--- src/main/java/com/example/demo/entity/Employee.java
+++ src/main/java/com/example/demo/entity/Employee.java
@@ -1,26 +1,42 @@
 package com.example.demo.entity;
-import jakarta.persistence.*;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "employee")
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="employee_id")
+    @Column(name = "employee_id")
     private Long employeeId;
 
-    @Column(name="employee_name")
+    @Column(name = "employee_name")
     private String employeeName;
 
-    @Column(name="password")
+    @Column(name = "password")
     private String password;
 
-
-    @Column(name="salary")
+    @Column(name = "salary")
     private long salary;
 
-    @Column(name="department")
+    @Column(name = "department")
     private String department;
+
+    public Employee() {
+    }
+
+    public Employee(Long employeeId, String employeeName, String password, long salary, String department) {
+        this.employeeId = employeeId;
+        this.employeeName = employeeName;
+        this.password = password;
+        this.salary = salary;
+        this.department = department;
+    }
 
     public Long getEmployeeId() {
         return employeeId;
@@ -41,6 +57,7 @@ public class Employee {
     public String getDepartment() {
         return department;
     }
+
     public void setEmployeeId(Long employeeId) {
         this.employeeId = employeeId;
     }
@@ -62,4 +79,3 @@ public class Employee {
     }
 
 }
-
